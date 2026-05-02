a64c35935c6545d3ee68d0939af986169dc42487
mikolajczyk <mikolajczyk.malgosia@gmail.com>
2026-05-02 23:56:53 +0200

    Rename text field component to input field

--- src/components/form/MetaFields.vue
+++ src/components/form/MetaFields.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { formLabels } from "../../config/formLabels";
 import type { EnvironmentConfig, FieldErrors, SituationForm } from "../../types/form";
-import TextField from "./TextField.vue";
+import InputField from "./InputField.vue";
 
 defineProps<{
   env: EnvironmentConfig;
@@ -12,10 +12,10 @@ defineProps<{
 
 <template>
   <div class="field-grid">
-    <TextField v-model="form.meta.date" :label="formLabels.meta.date" type="date" required :error="fieldErrors['meta.date']" />
-    <TextField v-model="form.meta.time" :label="formLabels.meta.time" type="time" required :error="fieldErrors['meta.time']" />
-    <TextField v-model="form.meta.place" :label="formLabels.meta.place" required :error="fieldErrors['meta.place']" />
-    <TextField v-model="form.meta.lead" :label="env.lead" required :error="fieldErrors['meta.lead']" />
-    <TextField v-model="form.meta.present" :label="formLabels.meta.present" />
+    <InputField v-model="form.meta.date" :label="formLabels.meta.date" type="date" required :error="fieldErrors['meta.date']" />
+    <InputField v-model="form.meta.time" :label="formLabels.meta.time" type="time" required :error="fieldErrors['meta.time']" />
+    <InputField v-model="form.meta.place" :label="formLabels.meta.place" required :error="fieldErrors['meta.place']" />
+    <InputField v-model="form.meta.lead" :label="env.lead" required :error="fieldErrors['meta.lead']" />
+    <InputField v-model="form.meta.present" :label="formLabels.meta.present" />
   </div>
 </template>
