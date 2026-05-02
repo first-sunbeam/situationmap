997093a09b132e786d29e24593515a8d2876afa8
mikolajczyk <mikolajczyk.malgosia@gmail.com>
2026-05-02 22:11:20 +0200

    Extract shared meta fields

---
+++ src/components/form/MetaFields.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { formLabels } from "../../config/formLabels";
+import type { EnvironmentConfig, FieldErrors, SituationForm } from "../../types/form";
+
+defineProps<{
+  env: EnvironmentConfig;
+  form: SituationForm;
+  fieldErrors: FieldErrors;
+}>();
+</script>
+
+<template>
+  <div class="field-grid">
+    <label class="field">
+      <span class="field-label">{{ formLabels.meta.date }} <span class="required-mark">*</span></span>
+      <input class="text-input" :class="{ invalid: fieldErrors['meta.date'] }" type="date" v-model="form.meta.date" />
+      <span v-if="fieldErrors['meta.date']" class="field-error">{{ fieldErrors['meta.date'] }}</span>
+    </label>
+
+    <label class="field">
+      <span class="field-label">{{ formLabels.meta.time }} <span class="required-mark">*</span></span>
+      <input class="text-input" :class="{ invalid: fieldErrors['meta.time'] }" type="time" v-model="form.meta.time" />
+      <span v-if="fieldErrors['meta.time']" class="field-error">{{ fieldErrors['meta.time'] }}</span>
+    </label>
+
+    <label class="field">
+      <span class="field-label">{{ formLabels.meta.place }} <span class="required-mark">*</span></span>
+      <input class="text-input" :class="{ invalid: fieldErrors['meta.place'] }" v-model="form.meta.place" />
+      <span v-if="fieldErrors['meta.place']" class="field-error">{{ fieldErrors['meta.place'] }}</span>
+    </label>
+
+    <label class="field">
+      <span class="field-label">{{ env.lead }} <span class="required-mark">*</span></span>
+      <input class="text-input" :class="{ invalid: fieldErrors['meta.lead'] }" v-model="form.meta.lead" />
+      <span v-if="fieldErrors['meta.lead']" class="field-error">{{ fieldErrors['meta.lead'] }}</span>
+    </label>
+
+    <label class="field full">
+      <span class="field-label">{{ formLabels.meta.present }}</span>
+      <input class="text-input" v-model="form.meta.present" />
+    </label>
+  </div>
+</template>
